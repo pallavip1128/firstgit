03642249277070f03676af96ac536b7a24d610d9
pallavip1128 <pallavip1128@gmail.com>
2023-01-02 05:26:36 -0800

    my third commit

---
+++ dom1.js
@@ -0,0 +1,23 @@
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 1234;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.images);
+// console.log(document.links);
+// console.log(document.forms[0]);
+
+
+headerTitle=document.getElementById('header-title');
+console.log(headerTitle);
+headerTitle.style.borderBottom='solid 3px #000';
+var items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+// items[1].style.fontWeight ='bold';
+items[2].style.backgroundColor = 'green'; 
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight ='bold';
+}
